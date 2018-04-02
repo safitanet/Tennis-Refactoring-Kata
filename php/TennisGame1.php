1617f7e2ceefce750c74ce179e1ca3b54e0ada09
Belén Ortega <belenortega@centraldereservas.com>
2018-04-02 20:27:33 +0200

    Add player class

--- php/TennisGame1.php
+++ php/TennisGame1.php
@@ -4,18 +4,18 @@ class TennisGame1 implements TennisGame
 {
     private $m_score1 = 0;
     private $m_score2 = 0;
-    private $player1Name = '';
-    private $player2Name = '';
+    private $player1;
+    private $player2;
 
     public function __construct($player1Name, $player2Name)
     {
-        $this->player1Name = $player1Name;
-        $this->player2Name = $player2Name;
+        $this->player1 = new Player($player1Name);
+        $this->player2 = new Player($player2Name);
     }
 
     public function wonPoint($playerName)
     {
-        if ($this->player1Name == $playerName) {
+        if ($this->player1->getName() == $playerName) {
             $this->m_score1++;
         } else {
             $this->m_score2++;
@@ -28,7 +28,7 @@ class TennisGame1 implements TennisGame
         if ($this->isTie()) {
             $score = Score::getDescriptionScoreTie($this->m_score1);
         } elseif ($this->theyAreOverThree()) {
-            $score = Score::getDescriptionForAdvantageOrEndedGame($this->getDiffScores());
+            $score = $this->getDescriptionForAdvantageOrEndedGame($this->getDiffScores());
         } else {
             $score = Score::getDescriptionScore($this->m_score1);
             $score .= "-";
@@ -61,7 +61,24 @@ class TennisGame1 implements TennisGame
         return $this->m_score1 - $this->m_score2;
     }
 
-
+    /**
+     * @param $minusResult
+     *
+     * @return string
+     */
+    private function getDescriptionForAdvantageOrEndedGame($minusResult): string
+    {
+        if ($minusResult == 1) {
+            $score = "Advantage " . $this->player1->getName();
+        } elseif ($minusResult == -1) {
+            $score = "Advantage " . $this->player2->getName();
+        } elseif ($minusResult >= 2) {
+            $score = "Win for " . $this->player1->getName();
+        } else {
+            $score = "Win for " . $this->player2->getName();
+        }
+        return $score;
+    }
 }
 
 class Score {
@@ -88,24 +105,22 @@ class Score {
         $score = $score > 2 ? 3 : $score;
         return Score::TIE_SCORE[$score];
     }
+}
 
-    /**
-     * @param $minusResult
-     *
-     * @return string
-     */
-    public static function getDescriptionForAdvantageOrEndedGame($minusResult): string
+class Player {
+    private $name;
+    private $score;
+
+    public function __construct($name)
     {
-        if ($minusResult == 1) {
-            $score = "Advantage player1";
-        } elseif ($minusResult == -1) {
-            $score = "Advantage player2";
-        } elseif ($minusResult >= 2) {
-            $score = "Win for player1";
-        } else {
-            $score = "Win for player2";
-        }
-        return $score;
+        $this->name = $name;
+        $this->score = 0;
     }
+
+    public function getName()
+    {
+       return $this->name;
+    }
+
 }
 
